dc3da868ee0d806830e23f86ebd87a690dc0795e
Thiago Cunha <thiago.santos.cunha@everis.com>
2020-10-27 14:29:31 -0300

    Preparando arquivo para teste e Adicionando Docker composer

--- src/test/java/br/com/bradesco/pfiningestion/jobs/IngestionJobTest.java
+++ src/test/java/br/com/bradesco/pfiningestion/jobs/IngestionJobTest.java
@@ -1,17 +1,63 @@
 package br.com.bradesco.pfiningestion.jobs;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.runner.RunWith;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobInstance;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.JobRepositoryTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@ContextConfiguration("application.properties")
+@SpringBatchTest
+@EnableAutoConfiguration
+@TestExecutionListeners({ 
+    DependencyInjectionTestExecutionListener.class, 
+    DirtiesContextTestExecutionListener.class}
+)
+@DirtiesContext(classMode = ClassMode.AFTER_CLASS)
 public class IngestionJobTest {
+
+    @Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils;
+
+    @Autowired
+    private JobRepositoryTestUtils jobRepositoryTestUtils;
 
     @BeforeEach
     void init() {
     }
 
-    @AfterEach
+    @After
     public void finalize() {
+        jobRepositoryTestUtils.removeJobExecutions();
+    }
+
+    @Test
+    public void deveExecutarUmJobComSucesso() throws Exception {
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob();
+
+        JobInstance actualJobInstance = jobExecution.getJobInstance();
+        ExitStatus actualJobExitStatus = jobExecution.getExitStatus();
+
+        assertEquals(actualJobInstance.getJobName(), is("ExecuteIngestionJob"));
+        assertEquals(actualJobExitStatus, is("COMPLETED"));
     }
 }
