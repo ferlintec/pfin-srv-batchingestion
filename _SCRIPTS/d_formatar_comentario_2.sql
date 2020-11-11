73194945b39d28e53d5b2b33044db2cae93b47bf
Thiago Cunha <thiago.santos.cunha@everis.com>
2020-11-10 22:16:08 -0300

    Criação da function do Step D

---
+++ _SCRIPTS/d_formatar_comentario_2.sql
@@ -0,0 +1,29 @@
+IF OBJECT_ID('getAssuntoMomentoVida', 'IF') IS NOT NULL
+BEGIN
+    DROP FUNCTION [dbo].[getAssuntoMomentoVida]
+END
+GO
+
+CREATE FUNCTION [dbo].[getAssuntoMomentoVida]()
+RETURNS TABLE
+AS
+RETURN
+    SELECT
+        [tAssntPlanjFincr].[nAssntPlanjFincr] as tAssntPlanjFincr_nAssntPlanjFincr,
+        [tAssntPlanjFincr].[nMomenVidaPlanjFincr] as tAssntPlanjFincr_nMomenVidaPlanjFincr,
+        [tAssntPlanjFincr].[nOrdExibcInfoPlanjFincr] as tAssntPlanjFincr_nOrdExibcInfoPlanjFincr,
+        [tAssntPlanjFincr].[rAssntPlanjFincr] as tAssntPlanjFincr_rAssntPlanjFincr,
+        [tMomenVidaPlanjFincr].[nMomenVidaPlanjFincr] as tMomenVidaPlanjFincr_nMomenVidaPlanjFincr,
+        [tMomenVidaPlanjFincr].[nOrdExibcInfoPlanjFincr] as tMomenVidaPlanjFincr_nOrdExibcInfoPlanjFincr,
+        [tMomenVidaPlanjFincr].[rMomenVidaPlanjFincr] as tMomenVidaPlanjFincr_rMomenVidaPlanjFincr
+    FROM [PFIND000].[dbo].[tAssntPlanjFincr] as tAssntPlanjFincr
+    INNER JOIN [PFIND000].[dbo].[tMomenVidaPlanjFincr] as tMomenVidaPlanjFincr
+        ON tMomenVidaPlanjFincr.nMomenVidaPlanjFincr = tAssntPlanjFincr.nMomenVidaPlanjFincr
+    ORDER BY tAssntPlanjFincr_nAssntPlanjFincr;
+GO
+
+
+
+--Precisa pegar o CTE do Step C e agregar a function acima
+
+
