504d7b094b2d60b090d5441d4570d04ec1387966
Adriano Ferlin <adferlin@everis.com>
2020-11-13 15:34:51 -0300

    Finalização do step 1, subdimensão de dependente.

--- _SCRIPTS/a_formatar_dependente.sql
+++ _SCRIPTS/a_formatar_dependente.sql
@@ -1,29 +1,47 @@
-IF OBJECT_ID('getAgenteMercadoDadosAgente', 'IF') IS NOT NULL
+--************************************************************************
+-- Function para retornar a lista de agente de mercado de 
+-- um dependente.
+-- Retorna os agentes de mercado em formato Json.
+--************************************************************************
+IF OBJECT_ID('getAgenteMercadoDadosAgenteByIdDependente', 'FN') IS NOT NULL
 BEGIN
-    DROP FUNCTION [dbo].[getAgenteMercadoDadosAgente]
+    DROP FUNCTION [dbo].[getAgenteMercadoDadosAgenteByIdDependente];
 END
 GO
+CREATE FUNCTION [dbo].[getAgenteMercadoDadosAgenteByIdDependente](@ID_DEPENDENTE INT)
+    RETURNS NVARCHAR
+    AS
+    BEGIN
+        --PRINT CONVERT(VARCHAR, @ID_DEPENDENTE);
+        DECLARE @JSON_FORMAT NVARCHAR(MAX);
+        SET @JSON_FORMAT = (SELECT
+            agteMercdDepdtPlanjFincr.nPssoaDepdtPlanjFincr AS tAgteMercdDepdtPlanjFincr_nPssoaDepdtPlanjFincr,
+            agteMercdDepdtPlanjFincr.nAgteMercdPlanjFincr AS tAgteMercdDepdtPlanjFincr_nAgteMercdPlanjFincr,
+            agteMercdPlanjFincr.nAgteMercdPlanjFincr AS tAgteMercdPlanjFincr_nAgteMercdPlanjFincr,
+            agteMercdPlanjFincr.cBco AS tAgteMercdPlanjFincr_cBco,
+            agteMercdPlanjFincr.iAgteMercdPlanjFincr AS tAgteMercdPlanjFincr_iAgteMercdPlanjFincr,
+            agteMercdPlanjFincr.cUsuarIncl AS tAgteMercdPlanjFincr_cUsuarIncl,
+            agteMercdPlanjFincr.hInclReg AS tAgteMercdPlanjFincr_hInclReg
+        FROM [dbo].[tAgteMercdDepdtPlanjFincr] AS agteMercdDepdtPlanjFincr
+        INNER JOIN [dbo].[tAgteMercdPlanjFincr] AS agteMercdPlanjFincr
+            ON agteMercdDepdtPlanjFincr.nAgteMercdPlanjFincr = agteMercdPlanjFincr.nAgteMercdPlanjFincr
+        WHERE agteMercdDepdtPlanjFincr.nPssoaDepdtPlanjFincr = @ID_DEPENDENTE
+        FOR JSON PATH);
 
-DECLARE @MyHierarchy Hierarchy;
-
-CREATE FUNCTION [dbo].[getAgenteMercadoDadosAgente]()
-RETURNS TABLE
-AS
-RETURN
-    SELECT
-        agteMercdDepdtPlanjFincr.nPssoaDepdtPlanjFincr AS tAgteMercdDepdtPlanjFincr_nPssoaDepdtPlanjFincr,
-        agteMercdDepdtPlanjFincr.nAgteMercdPlanjFincr AS tAgteMercdDepdtPlanjFincr_nAgteMercdPlanjFincr,
-        agteMercdPlanjFincr.nAgteMercdPlanjFincr AS tAgteMercdPlanjFincr_nAgteMercdPlanjFincr,
-        agteMercdPlanjFincr.cBco AS tAgteMercdPlanjFincr_cBco,
-        agteMercdPlanjFincr.iAgteMercdPlanjFincr AS tAgteMercdPlanjFincr_iAgteMercdPlanjFincr,
-        agteMercdPlanjFincr.cUsuarIncl AS tAgteMercdPlanjFincr_cUsuarIncl,
-        agteMercdPlanjFincr.hInclReg AS tAgteMercdPlanjFincr_hInclReg
-    FROM [dbo].[tAgteMercdDepdtPlanjFincr] AS agteMercdDepdtPlanjFincr
-    INNER JOIN [dbo].[tAgteMercdPlanjFincr] AS agteMercdPlanjFincr
-        ON agteMercdDepdtPlanjFincr.nAgteMercdPlanjFincr = agteMercdPlanjFincr.nAgteMercdPlanjFincr;
+        RETURN @JSON_FORMAT;
+    END
 GO
 
-CREATE FUNCTION [dbo].[getFormatarDependente]()
+
+--************************************************************************
+-- Function para retornar a lista de dependentes
+--************************************************************************
+IF OBJECT_ID('getAllDependentes', 'IF') IS NOT NULL
+BEGIN
+    DROP FUNCTION [dbo].[getAllDependentes]
+END
+GO
+CREATE FUNCTION [dbo].[getAllDependentes]()
 RETURNS TABLE
 AS
 RETURN
@@ -43,14 +61,141 @@ RETURN
         [hInclReg] as tPssoaDepdtPlanjFincr_hInclReg,
         [hUltAltReg] as tPssoaDepdtPlanjFincr_hUltAltReg
     FROM [dbo].[tPssoaDepdtPlanjFincr] as tPssoaDepdtPlanjFincr
-    ORDER BY tPssoaDepdtPlanjFincr.nPssoaDepdtPlanjFincr;
+   ;
 GO
 
---Precisa fazer o cursor e juntar com a function acima
+--Declaração da tabela temporária de dependente
+DECLARE @DEPENDENTE_TABLE TABLE (
+        tPssoaDepdtPlanjFincr_nPssoaDepdtPlanjFincr INT,
+        tPssoaDepdtPlanjFincr_nPssoaPlanjFincr INT,
+        tPssoaDepdtPlanjFincr_cCategVnclo [decimal](5, 0),
+        tPssoaDepdtPlanjFincr_cTpoVnclo [decimal](2, 0),
+        tPssoaDepdtPlanjFincr_cIndcdDepdcPssoa [decimal](1, 0),
+        tPssoaDepdtPlanjFincr_cCpfDepdtPssoaPlanjFincr INT,
+        tPssoaDepdtPlanjFincr_cCtrlCpfDepdtPssoaPlanjFincr TINYINT,
+        tPssoaDepdtPlanjFincr_dNascDepdtPssoaPlanjFincr DATETIME,
+        tPssoaDepdtPlanjFincr_iPssoaDepdtPlanjFincr VARCHAR(70),
+        tPssoaDepdtPlanjFincr_rInfoFincrDepdt VARCHAR(255),
+        tPssoaDepdtPlanjFincr_cSitReg [DECIMAL](1,0),
+        tPssoaDepdtPlanjFincr_cUsuarIncl [CHAR](9),
+        tPssoaDepdtPlanjFincr_hInclReg DATETIME,
+        tPssoaDepdtPlanjFincr_hUltAltReg DATETIME,
+        tAgteMercdDepdtPlanjFincr_json NVARCHAR(MAX)
+)
+ 
+--Variáveis temporárias
+DECLARE @tPssoaDepdtPlanjFincr_nPssoaDepdtPlanjFincr INT,
+        @tPssoaDepdtPlanjFincr_nPssoaPlanjFincr INT,
+        @tPssoaDepdtPlanjFincr_cCategVnclo [decimal](5, 0),
+        @tPssoaDepdtPlanjFincr_cTpoVnclo [decimal](2, 0),
+        @tPssoaDepdtPlanjFincr_cIndcdDepdcPssoa [decimal](1, 0),
+        @tPssoaDepdtPlanjFincr_cCpfDepdtPssoaPlanjFincr INT,
+        @tPssoaDepdtPlanjFincr_cCtrlCpfDepdtPssoaPlanjFincr TINYINT,
+        @tPssoaDepdtPlanjFincr_dNascDepdtPssoaPlanjFincr DATETIME,
+        @@tPssoaDepdtPlanjFincr_iPssoaDepdtPlanjFincr VARCHAR(70),
+        @tPssoaDepdtPlanjFincr_rInfoFincrDepdt VARCHAR(255),
+        @tPssoaDepdtPlanjFincr_cSitReg [DECIMAL](1,0),
+        @tPssoaDepdtPlanjFincr_cUsuarIncl [CHAR](9),
+        @tPssoaDepdtPlanjFincr_hInclReg DATETIME,
+        @tPssoaDepdtPlanjFincr_hUltAltReg DATETIME,
+        @tAgteMercdDepdtPlanjFincr_json NVARCHAR(MAX)
 
 
+-- ****************************************************************************
+-- Criação do cursor para alimentar a tabela temporária com
+-- os dados dos dependentes e seus agentes de mercado.
+-- ****************************************************************************
 
-select * from getAgenteMercadoDadosAgente() agtMerDAgt;
+DECLARE  CURSOR_DEPENDENTES CURSOR FOR select * from dbo.getAllDependentes() ORDER BY tPssoaDepdtPlanjFincr_nPssoaPlanjFincr;
 
+OPEN CURSOR_DEPENDENTES
+    FETCH NEXT FROM CURSOR_DEPENDENTES INTO @tPssoaDepdtPlanjFincr_nPssoaDepdtPlanjFincr,
+        @tPssoaDepdtPlanjFincr_nPssoaPlanjFincr,
+        @tPssoaDepdtPlanjFincr_cCategVnclo,
+        @tPssoaDepdtPlanjFincr_cTpoVnclo,
+        @tPssoaDepdtPlanjFincr_cIndcdDepdcPssoa,
+        @tPssoaDepdtPlanjFincr_cCpfDepdtPssoaPlanjFincr,
+        @tPssoaDepdtPlanjFincr_cCtrlCpfDepdtPssoaPlanjFincr,
+        @tPssoaDepdtPlanjFincr_dNascDepdtPssoaPlanjFincr,
+        @@tPssoaDepdtPlanjFincr_iPssoaDepdtPlanjFincr,
+        @tPssoaDepdtPlanjFincr_rInfoFincrDepdt,
+        @tPssoaDepdtPlanjFincr_cSitReg,
+        @tPssoaDepdtPlanjFincr_cUsuarIncl,
+        @tPssoaDepdtPlanjFincr_hInclReg,
+        @tPssoaDepdtPlanjFincr_hUltAltReg;    
 
+        
+    WHILE @@FETCH_STATUS = 0
+    BEGIN
 
+        --Busca os agentes do dependente, e converte em formato Json para armazenar.            
+        --SET @tAgteMercdDepdtPlanjFincr_json = dbo.getAgenteMercadoDadosAgenteByIdDependente(@tPssoaDepdtPlanjFincr_nPssoaDepdtPlanjFincr);
+
+        SET @tAgteMercdDepdtPlanjFincr_json = (SELECT
+                            agteMercdDepdtPlanjFincr.nPssoaDepdtPlanjFincr AS tAgteMercdDepdtPlanjFincr_nPssoaDepdtPlanjFincr,
+                            agteMercdDepdtPlanjFincr.nAgteMercdPlanjFincr AS tAgteMercdDepdtPlanjFincr_nAgteMercdPlanjFincr,
+                            agteMercdPlanjFincr.nAgteMercdPlanjFincr AS tAgteMercdPlanjFincr_nAgteMercdPlanjFincr,
+                            agteMercdPlanjFincr.cBco AS tAgteMercdPlanjFincr_cBco,
+                            agteMercdPlanjFincr.iAgteMercdPlanjFincr AS tAgteMercdPlanjFincr_iAgteMercdPlanjFincr,
+                            agteMercdPlanjFincr.cUsuarIncl AS tAgteMercdPlanjFincr_cUsuarIncl,
+                            agteMercdPlanjFincr.hInclReg AS tAgteMercdPlanjFincr_hInclReg
+                        FROM [dbo].[tAgteMercdDepdtPlanjFincr] AS agteMercdDepdtPlanjFincr
+                        INNER JOIN [dbo].[tAgteMercdPlanjFincr] AS agteMercdPlanjFincr
+                            ON agteMercdDepdtPlanjFincr.nAgteMercdPlanjFincr = agteMercdPlanjFincr.nAgteMercdPlanjFincr
+                        WHERE agteMercdDepdtPlanjFincr.nPssoaDepdtPlanjFincr = @tPssoaDepdtPlanjFincr_nPssoaDepdtPlanjFincr
+                        FOR JSON PATH);
+
+        INSERT INTO @DEPENDENTE_TABLE  (
+            tPssoaDepdtPlanjFincr_nPssoaDepdtPlanjFincr,
+            tPssoaDepdtPlanjFincr_nPssoaPlanjFincr,
+            tPssoaDepdtPlanjFincr_cCategVnclo,
+            tPssoaDepdtPlanjFincr_cTpoVnclo,
+            tPssoaDepdtPlanjFincr_cIndcdDepdcPssoa,
+            tPssoaDepdtPlanjFincr_cCpfDepdtPssoaPlanjFincr,
+            tPssoaDepdtPlanjFincr_cCtrlCpfDepdtPssoaPlanjFincr,
+            tPssoaDepdtPlanjFincr_dNascDepdtPssoaPlanjFincr,
+            tPssoaDepdtPlanjFincr_iPssoaDepdtPlanjFincr,
+            tPssoaDepdtPlanjFincr_rInfoFincrDepdt,
+            tPssoaDepdtPlanjFincr_cSitReg,
+            tPssoaDepdtPlanjFincr_cUsuarIncl,
+            tPssoaDepdtPlanjFincr_hInclReg,
+            tPssoaDepdtPlanjFincr_hUltAltReg,
+            tAgteMercdDepdtPlanjFincr_json) 
+            VALUES 
+            (@tPssoaDepdtPlanjFincr_nPssoaDepdtPlanjFincr,
+            @tPssoaDepdtPlanjFincr_nPssoaPlanjFincr,
+            @tPssoaDepdtPlanjFincr_cCategVnclo,
+            @tPssoaDepdtPlanjFincr_cTpoVnclo,
+            @tPssoaDepdtPlanjFincr_cIndcdDepdcPssoa,
+            @tPssoaDepdtPlanjFincr_cCpfDepdtPssoaPlanjFincr,
+            @tPssoaDepdtPlanjFincr_cCtrlCpfDepdtPssoaPlanjFincr,
+            @tPssoaDepdtPlanjFincr_dNascDepdtPssoaPlanjFincr,
+            @@tPssoaDepdtPlanjFincr_iPssoaDepdtPlanjFincr,
+            @tPssoaDepdtPlanjFincr_rInfoFincrDepdt,
+            @tPssoaDepdtPlanjFincr_cSitReg,
+            @tPssoaDepdtPlanjFincr_cUsuarIncl,
+            @tPssoaDepdtPlanjFincr_hInclReg,
+            @tPssoaDepdtPlanjFincr_hUltAltReg,
+            @tAgteMercdDepdtPlanjFincr_json);
+
+        FETCH NEXT FROM CURSOR_DEPENDENTES INTO @tPssoaDepdtPlanjFincr_nPssoaDepdtPlanjFincr,
+            @tPssoaDepdtPlanjFincr_nPssoaPlanjFincr,
+            @tPssoaDepdtPlanjFincr_cCategVnclo,
+            @tPssoaDepdtPlanjFincr_cTpoVnclo,
+            @tPssoaDepdtPlanjFincr_cIndcdDepdcPssoa,
+            @tPssoaDepdtPlanjFincr_cCpfDepdtPssoaPlanjFincr,
+            @tPssoaDepdtPlanjFincr_cCtrlCpfDepdtPssoaPlanjFincr,
+            @tPssoaDepdtPlanjFincr_dNascDepdtPssoaPlanjFincr,
+            @@tPssoaDepdtPlanjFincr_iPssoaDepdtPlanjFincr,
+            @tPssoaDepdtPlanjFincr_rInfoFincrDepdt,
+            @tPssoaDepdtPlanjFincr_cSitReg,
+            @tPssoaDepdtPlanjFincr_cUsuarIncl,
+            @tPssoaDepdtPlanjFincr_hInclReg,
+            @tPssoaDepdtPlanjFincr_hUltAltReg;
+            
+    END;
+
+CLOSE CURSOR_DEPENDENTES  
+DEALLOCATE CURSOR_DEPENDENTES
+
+--SELECT * FROM @DEPENDENTE_TABLE;
